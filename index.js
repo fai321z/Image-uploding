53906500c6b1390f7c32487c775612ff9b552eae
fai321z <faizank432h@gmail.com>
2024-12-16 23:16:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const coll = require("./model/mongo");
+const coll = require("./model/mongo");
 const cors = require('cors')
 const Path = require('path');
 const fs= require('fs')
@@ -14,19 +14,21 @@ app.use(express.urlencoded({ extended:true}));
 app.use(cors())
 const storage = multer.diskStorage({
     destination: (req,file,cb)=>{
-        cb(null,Path.join(__dirname, 'public/images'))
+        cb(null,'public/images')
     },
     filename:(req,file,cb)=>{
         cb(null,file.originalname)
     }
 })
 const upload=multer({
-    storage
+    storage:storage
 })
 
 
-app.post("/api/create",upload.single('file'),(req,res,)=>{
-    console.log(req.file)
+app.post("/api/create",upload.single('file'),(req,res)=>{
+    coll.create({image:req.file.filename})
+    .then(result=>res.json(result))
+    .catch(err=>console.log(err))
 
 })
 app.listen(port,()=>{
